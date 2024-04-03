14c7879a65b356b6a54ab27f6f894e4ff9c13127
Hoang Tran <hoatran@student.hive.fi>
2024-04-03 18:01:13 +0300

    Added static helper function

--- string/ft_parse_cmd.c
+++ string/ft_parse_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:53:07 by hoatran           #+#    #+#             */
-/*   Updated: 2024/04/02 00:52:34 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/04/03 17:28:47 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,26 @@ static void	handle_quote(char *str)
 	}
 }
 
+static void	remove_open_end_quotes(char **str)
+{
+	size_t	length;
+	char	*temp;
+
+	if (str == NULL || *str == NULL || *str == '\0')
+		return ;
+	length = ft_strlen(*str);
+	if (**str != 34 && **str != 39)
+		return ;
+	if ((*str)[0] != (*str)[length - 1])
+		return ;
+	temp = (char *)malloc((length - 1) * sizeof(char));
+	if (temp == NULL)
+		return ;
+	ft_strlcpy(temp, *str + 1, length - 1);
+	free(*str);
+	*str = temp;
+}
+
 static char	**split_cmd(char *cmd)
 {
 	const char	patterns[2] = {31, '\0'};
@@ -51,6 +71,7 @@ static char	**split_cmd(char *cmd)
 		i = 0;
 		while (tokens[i] != NULL)
 		{
+			remove_open_end_quotes(&tokens[i]);
 			ft_replace(tokens[i], patterns, ' ');
 			i++;
 		}
