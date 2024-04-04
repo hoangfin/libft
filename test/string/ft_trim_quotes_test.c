679db3cefb16f219d7e537a43955724353a7b385
Hoang Tran <hoatran@student.hive.fi>
2024-04-04 13:54:56 +0300

    Initial commit

---
+++ test/string/ft_trim_quotes_test.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_trim_quotes_test.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 12:16:58 by hoatran           #+#    #+#             */
+/*   Updated: 2024/04/04 13:53:23 by hoatran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char case_1[] = "'\"Hello\"'";
+	char case_2[] = "'\"Hello\"";
+	char case_3[] = "''";
+	char case_4[] = "'";
+
+	ft_trim_quotes(case_1);
+	assert(strcmp(case_1, "Hello") == 0);
+
+	ft_trim_quotes(case_2);
+	assert(strcmp(case_2, "\'\"Hello\"") == 0);
+
+	ft_trim_quotes(case_3);
+	assert(strcmp(case_3, "") == 0);
+
+	ft_trim_quotes(case_4);
+	assert(strcmp(case_4, "'") == 0);
+	printf("All tests passed!\n");
+	return (EXIT_SUCCESS);
+}
