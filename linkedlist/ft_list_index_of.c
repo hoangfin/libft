e74cce0b48fc377ae182cd4cf50fcb337507a8eb
Hoang Tran <hoang.tran.fin@gmail.com>
2024-04-23 23:40:12 +0300

    Supported ft_list_index_of()

--- linkedlist/ft_list_index_of.c
+++ linkedlist/ft_list_index_of.c
@@ -6,13 +6,25 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 17:18:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/04/23 17:19:52 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/04/23 23:26:40 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_list.h"
 
-int	ft_list_index_of(t_list *list, void *data)
+int	ft_list_index_of(t_list *list, void *data, t_bool (*pre)(t_node *, void *))
 {
-	
+	int		i;
+	t_node	*node;
+
+	i = 0;
+	node = list->head;
+	while (i < (int)list->length)
+	{
+		if (pre(node, data))
+			return (i);
+		node = node->next;
+		i++;
+	}
+	return (-1);
 }
