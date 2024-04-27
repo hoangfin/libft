0a47b6a5a4a8dc4fba881f7f17b6538486ebabd7
Hoang Tran <hoang.tran.fin@gmail.com>
2024-04-28 02:50:58 +0300

    Added static function find_quote()

--- string/ft_parse_cmd.c
+++ string/ft_parse_cmd.c
@@ -6,35 +6,41 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:53:07 by hoatran           #+#    #+#             */
-/*   Updated: 2024/04/17 16:53:04 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/04/28 02:46:09 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_string.h"
 
+static char	*find_quote(char *str)
+{
+	char	prev;
+
+	prev = '\0';
+	while (*str != '\0')
+	{
+		if ((*str == 34 || *str == 39) && prev != '\\')
+			return (str);
+		prev = *str;
+		str++;
+	}
+	return (NULL);
+}
+
 static void	handle_quote(char *str)
 {
+	char	*open_quote;
 	char	*end_quote;
 
-	while (*str != '\0' && *str != 34 && *str != 39)
-		str++;
-	if (*str == '\0')
+	open_quote = find_quote(str);
+	if (open_quote == NULL)
 		return ;
-	end_quote = str + 1;
-	while (*end_quote != '\0' && *end_quote != *str)
-		end_quote++;
-	if (*end_quote == '\0')
-		handle_quote(str + 1);
-	else
-	{
-		while (str != end_quote)
-		{
-			if (ft_isspace(*str))
-				*str = 31;
-			str++;
-		}
-		handle_quote(str + 1);
-	}
+	end_quote = find_quote(open_quote + 1);
+	if (end_quote == NULL)
+		return ;
+	str = end_quote + 1;
+	ft_replace(open_quote, end_quote - open_quote + 1, "\f\n\r\t\v ", 31);
+	handle_quote(str);
 }
 
 static char	**split_cmd(char *cmd)
@@ -44,7 +50,7 @@ static char	**split_cmd(char *cmd)
 	int			i;
 
 	handle_quote(cmd);
-	ft_replace(cmd, "\f\n\r\t\v", ' ');
+	ft_replace(cmd, ft_strlen(cmd), "\f\n\r\t\v", ' ');
 	tokens = ft_split(cmd, ' ');
 	if (tokens != NULL)
 	{
@@ -52,7 +58,7 @@ static char	**split_cmd(char *cmd)
 		while (tokens[i] != NULL)
 		{
 			ft_trim_quotes(tokens[i]);
-			ft_replace(tokens[i], patterns, ' ');
+			ft_replace(tokens[i], ft_strlen(tokens[i]), patterns, ' ');
 			i++;
 		}
 	}
