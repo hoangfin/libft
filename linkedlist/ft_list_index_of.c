a0d3f80f074474ffd1236e1daf5c1693c796aae1
Hoang Tran <hoatran@student.hive.fi>
2024-05-14 16:59:02 +0300

    Fixed norminette

--- linkedlist/ft_list_index_of.c
+++ linkedlist/ft_list_index_of.c
@@ -6,14 +6,13 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 17:18:59 by hoatran           #+#    #+#             */
-/*   Updated: 2024/04/24 14:11:07 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/05/14 16:47:48 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_list.h"
 
-int	ft_list_index_of(t_list *list, void *data, t_bool (*pred)(t_node *, void *)
-)
+int	ft_list_index_of(t_list *list, void *data, t_bool (*pred)(t_node *, void *))
 {
 	int		i;
 	t_node	*node;
