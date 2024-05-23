29e8dc0ffa0e37038e8c103c74a021127201c419
Hoang Tran <hoatran@student.hive.fi>
2024-05-23 12:23:06 +0300

    Fixed unquote command input

--- string/ft_parse_cmd.c
+++ string/ft_parse_cmd.c
@@ -6,11 +6,38 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:53:07 by hoatran           #+#    #+#             */
-/*   Updated: 2024/04/28 17:27:58 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/05/23 12:21:28 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_string.h"
+
+static void	unquote(char *str)
+{
+	int		i;
+	int		j;
+	char	quote;
+
+	i = 0;
+	j = 0;
+	quote = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			if (quote == 0)
+				quote = str[i];
+			else if (quote == str[i])
+				quote = 0;
+			else
+				str[j++] = str[i];
+		}
+		else
+			str[j++] = str[i];
+		i++;
+	}
+	str[j] = '\0';
+}
 
 static char	*find_quote(char *str)
 {
@@ -58,7 +85,7 @@ static char	**split_cmd(char *cmd)
 		i = 0;
 		while (tokens[i] != NULL)
 		{
-			ft_trim_quotes(tokens[i]);
+			unquote(tokens[i]);
 			ft_trim(tokens[i], '\\');
 			ft_replace(tokens[i], ft_strlen(tokens[i]), "\x1F", ' ');
 			i++;
