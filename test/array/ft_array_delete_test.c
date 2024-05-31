167e96a4eb68390d395e310294202ab209767fb9
Hoang Tran <hoang.tran.fin@gmail.com>
2024-05-31 22:36:16 +0300

    Initial commid

---
+++ test/array/ft_array_delete_test.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char **command = (char **)malloc(4 * sizeof(char *));
+	command[0] = ft_strdup("echo");
+	command[1] = ft_strdup("abc");
+	command[2] = ft_strdup("def");
+	command[3] = NULL;
+	printf("%s\n", command[2]);
+	ft_array_delete((void **)&command, sizeof(char *), 4, free);
+	printf("%d\n", command == NULL);
+
+	int *numbers = (int *)malloc(4 * sizeof(int));
+	numbers[0] = 0;
+	numbers[1] = 1;
+	numbers[2] = 2;
+	numbers[3] = 3;
+	ft_array_delete((void **)&numbers, sizeof(int), 4, NULL);
+	printf("%d\n", numbers == NULL);
+	return (0);
+}
