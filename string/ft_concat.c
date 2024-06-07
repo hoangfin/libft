8b1c9280f660b7ccf62242d65bda7fff4329d21a
Hoang Tran <hoang.tran.fin@gmail.com>
2024-06-07 22:53:02 +0300

    Used ft_join_strings() in implementation

--- string/ft_concat.c
+++ string/ft_concat.c
@@ -6,30 +6,33 @@
 /*   By: hoatran <hoatran@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 20:13:01 by hoatran           #+#    #+#             */
-/*   Updated: 2024/05/22 20:18:58 by hoatran          ###   ########.fr       */
+/*   Updated: 2024/06/07 22:22:52 by hoatran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_string.h"
 
-int	ft_concat(char **target, const char *str)
+int	ft_concat(char **target, int string_count, ...)
 {
-	char	*temp;
+	const char	*temp = *target;
+	va_list		args;
+	char		*str;
 
+	va_start(args, string_count);
+	str = ft_join_strings(string_count, args);
+	va_end(args);
+	if (str == NULL)
+		return (-1);
+	if (*target == NULL)
+		*target = str;
+	else
+		*target = ft_strjoin(*target, str);
+	free(str);
 	if (*target == NULL)
 	{
-		*target = ft_strdup(str);
-		if (*target == NULL)
-			return (-1);
-		return (0);
-	}
-	temp = *target;
-	*target = ft_strjoin(*target, str);
-	if (*target == NULL)
-	{
-		*target = temp;
+		*target = (char *)temp;
 		return (-1);
 	}
-	free(temp);
+	free((void *)temp);
 	return (0);
 }
